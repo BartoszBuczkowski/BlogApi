9a01635d22286740fbe97a87b7d7b59a6afaf7b9
bartosz.buczkowski <bartosz.buczkowski@reasonapps.pl>
2020-08-26 21:41:12 +0200

    Mail transporter has been changed. Email account values will be pulled from .env

--- api/email/transporter.js
+++ api/email/transporter.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-           user: 'firanroom@gmail.com',
-           pass: 'Eloelo320'
+           user: process.env.SERVICE_EMAIL_ADDRESS, 
+           pass: process.env.SERVICE_EMAIL_PASSWORD
        }
    });
 
